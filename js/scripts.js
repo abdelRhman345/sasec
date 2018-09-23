cfb03f2e9b835c48481cc23136a14bc611d40ffd
abdelRhman345 <abrhman517@gmail.com>
2018-09-23 21:22:01 +0200

    sasec

---
+++ js/scripts.js
@@ -0,0 +1,76 @@
+/*global $*/
+$(function () {
+    "use strict";
+    
+    // Carousel 
+    $('.carousel').carousel({
+        interval: 5000
+    });
+
+    $('.icon-search').click(function () {
+        $('.name-search').slideToggle(300);
+    });
+
+    $('.dropdown .nav-link').click(function () {
+        $('.dropdown-menu').slideToggle(300);
+    });
+
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".nav-links .links-item .link").on('click', function (event) {
+
+        // Prevent default anchor click behavior
+          event.preventDefault();
+  
+        // Store hash
+          var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+              scrollTop: $(hash).offset().top - 120
+          }, 1500, function () {
+  
+            // Add hash (#) to URL when done scrolling (default click behavior)
+              window.location.hash = hash;
+  
+          });
+    });
+
+    // Check to see if the window is top if not then fixed Navbar
+    
+    $(window).scroll(function () {
+        
+        var sc = $(window).scrollTop();
+        
+        if (sc >= 35) {
+            
+            $('.navbar').css({
+                "position": 'fixed',
+                'top': '0px'
+            });
+        
+            
+        } else {
+            
+            $('.navbar').css({
+                "position": 'relative',
+            });
+            
+        }
+        
+    });
+});
+
+// Loading Page 
+$(window).on('load', function () {
+   
+    "use strict";
+    
+    $('.loading').fadeOut(2000, function () {
+        
+        $(this).remove();
+        
+        $('body').css("overflow", "auto");
+    });
+    
+});
